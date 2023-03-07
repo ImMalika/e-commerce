f70f8bb30ba5393191cf6430f5521f511c521a14
ImMalika <qmalika5450@gmail.com>
2023-03-07 16:01:44 +0500

    for new changes

---
+++ src/components/cardProd/cardProd.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import c from "./cardProd.module.scss";
+
+const CardProd = () => {
+  return (
+    <div className={c.CardProd}>
+      Card
+    </div>
+  )
+}
+
+export default CardProd
--- src/components/categs/categs.js
+++ src/components/categs/categs.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import c from "./categs.module.scss";
+import { UseGetData } from '../../utils/hooks/getData';
 
 const Categs = ({ categTitle }) => {
+    const allcategs = UseGetData(["categories"], '/categories');
     return (
         <div className={c.categswrapper}>
-            
             <button className={c.categTitle}>
                 {categTitle}
             </button>
