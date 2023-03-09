2866c0c20c00f2799e753497171de41bf8bd8082
ImMalika <qmalika5450@gmail.com>
2023-03-09 14:30:19 +0500

    for new pages

--- src/pages/info/info.js
+++ src/pages/info/info.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import c from "./info.module.scss"
-
+import Img from '../../components/img/img';
 
 const Info = () => {
   return (
-    <div className={c.info}>Info</div>
+    <div className={c.info}>
+
+      <Img imgUrl={'https://www.dbs.com/iwov-resources/images/foundation/about-us/contact-hero.jpg'}/>
+    </div>
   )
 }
 
-export default Info
+export default Info;
--- src/pages/message/message.js
+++ src/pages/message/message.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import c from "./message.module.scss"
+import c from "./message.module.scss";
+import Img from '../../components/img/img';
 
 const Message = () => {
   return (
-    <div className={c.message}>Message</div>
+    <div className={c.message}>
+      <Img imgUrl={'https://it-tehnik.ru/wp-content/uploads/11-39.jpg'}/>
+    </div>
   )
 }
 
