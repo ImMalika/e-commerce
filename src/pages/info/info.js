930fb673a9cbb6a23191e4e9de3f2a6ee4d08aa7
Mekhrangiz <yangiboeva.m7@gmail.com>
2023-03-09 18:07:17 +0500

    a

--- src/pages/info/info.js
+++ src/pages/info/info.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import c from "./info.module.scss"
+import  { useState } from 'react';
 
-
-const Info = () => {
+const Info = ({phone,address,addressMap,email,telegram,instagram}) => {
   return (
-    <div className={c.info}>Info</div>
+    <div className={c.info}>
+      <a href='tel:+99890 1234567890' ><div className={c.phoneN}>Phone <i class="fa-solid fa-phone"></i>{phone}</div></a> 
+      <a><div className={c.address}>Address <i class="fa-solid fa-location-pin"></i>{address}</div> </a>
+   
+      <a href='mailto:jane@example.com'>  <div className={c.emaill}>Email <i class="fa-solid fa-envelope"></i>{email}</div></a>
+      <a href='https://t.me/@jane_doe'>  <div className={c.telegramm}>Telegram <i class="fa-brands fa-telegram"></i>{telegram}</div></a>
+      <a href='https://www.instagram.com/@jane_doe'> <div className={c.instagramm}>Instagram <i class="fa-brands fa-instagram"></i>{instagram}</div></a>
+       <div className={c.addresM}>Map <i class="fa-solid fa-map"></i>{addressMap}</div>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.9150265443895!2d69.26224751528784!3d41.3324610792693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b6cbd7e49a1%3A0xf23c3817c486d743!2sAmity%20University%20Tashkent!5e0!3m2!1sru!2s!4v1678360915981!5m2!1sru!2s" width={600} height={450} border={0} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
   )
 }
 
 export default Info
+
+
+
--- src/pages/message/message.js
+++ src/pages/message/message.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import c from "./message.module.scss"
-
-const Message = () => {
+import {useForm} from "react-hook-form";
+import c from "./message.module.scss";
+function Message() {
+  const {register,handleSubmit,formState:{errors}}=useForm();
+const send=(data)=>{
+  console.log(data);
+}
+console.log(errors);
   return (
-    <div className={c.message}>Message</div>
+    <div className={c.div}>
+      <h1 className={c.h1}>Message Us</h1>
+      <form className={c.form} onSubmit={handleSubmit(send)}>
+        <input className={c.input} type="tel" placeholder="phone Number"{...register("tel",{required:true})} />
+        <input className={c.input} type="text" placeholder="subject" {...register("subject",{required:true})}/>
+        <input className={c.input} type="message" placeholder="message" {...register("message",{required:true})}/>
+        <p>Status:PENDING</p>
+        <button className={c.button}>Send</button>
+      </form>
+    </div>
   )
 }
 
 export default Message
+
+
